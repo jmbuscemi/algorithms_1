2ff26b03d8b7863aeeedba1ae608185f2816b207
John M Buscemi <jmbuscemi@gmail.com>
2016-12-10 13:40:06 -0500

    Add random number seeding to the input array

--- run.go
+++ run.go
@@ -3,13 +3,19 @@ package main
 import (
 	"coursera_algorithms_1/week_1"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 //return if array has 0 or 1 elements
 //if array has odd lengths, then break it into two pieces (n-1)/2 and (n+1)/2
 
 func main() {
-	slice := []int{4, 75, 3, 765, 9, 3, 2, 2, 3, 23, 987, 74, 100, 8747, 5, 6, 2, 0}
+	slice := []int{}
+	rand.Seed(time.Now().Unix())
+	for i := 0; i < 21; i++ {
+		slice = append(slice, rand.Int())
+	}
 
 	fmt.Printf("%v\n%v\n", slice, mergesort.MergeSort(slice))
 }
